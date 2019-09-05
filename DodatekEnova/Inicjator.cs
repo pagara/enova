bd282807bf86e5645b8615d7bc3170fcd3ac22e4
Przemyslaw Gara <przemyslaw.gara@gmail.com>
2019-09-05 15:43:56 +0100

    Add project files.

---
+++ DodatekEnova/Inicjator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Soneta.Business;
+
+[assembly: ProgramInitializer(typeof(DodatekEnova.Inicjator))]
+
+namespace DodatekEnova
+{
+    class Inicjator : IProgramInitializer
+    {
+        void IProgramInitializer.Initialize()
+        {
+            Debugger.Log(0, "", "Inicjacja mojego dodatku");
+          //  Debug.WriteLine("Inicjacja mojego dodatku");
+        }
+    }
+}
